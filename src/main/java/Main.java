b638aaf800c605321db983cb5ad37c0ed6dbe7ab
profKalil <84177112+profKalil@users.noreply.github.com>
2024-03-15 12:42:52 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+
+    // Variáveis
+    String mensagem = "Ola mundo";
+
+    // Processos
+
+
+    //Saídas    
+    System.out.println(mensagem);
+    System.out.print(mensagem);
+    System.out.print(mensagem);
+    System.out.print(mensagem);
+    System.out.print(mensagem);
+}
+}
